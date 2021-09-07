692c0b270e0d9f0fde0118d4b95dce08330decd1
sameesh yadav <sameeshyadav@gmail.com>
2021-09-07 16:59:31 +0530

    Admin dashbord structure

--- sourceCode/frontend/src/Components/Dashboard/Dashboard.js
+++ sourceCode/frontend/src/Components/Dashboard/Dashboard.js
@@ -3,8 +3,9 @@ export default function Dashboard() {
     return (
         <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
             <h1>
-            <a href="login">Login</a> &nbsp;
-            <a href="cart">Cart</a>
+                <a href="AdminLogin">AdminLogin</a>&nbsp;
+                <a href="login">Login</a>&nbsp;
+                <a href="cart">Cart</a>
             </h1>
         </div>
     );
---
+++ sourceCode/frontend/src/Components/Login/AdminLogin.js
@@ -0,0 +1,8 @@
+
+export default function AdminLogin() {
+    return (
+        <div>
+            <h1>AdminLogin</h1>
+        </div>
+    );
+}
